84c970bf5163401cf75d22ba78a3def0265a118c
Danilo <danilo.nascimento2@hotmail.com>
2023-06-08 13:26:02 -0300

    Validate token for the users logged in

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from "@/components/TheFooter.vue";
 import { RouterView } from 'vue-router';
+import { api } from "./services";
 
 
 export default {
@@ -9,6 +10,16 @@ export default {
     TheHeader,
     TheFooter,
     RouterView
+  },
+  created(){
+    if(window.localStorage.token) {
+      api.validateToken().then(() => {
+        this.$store.dispatch("getUser");
+      // eslint-disable-next-line no-unused-vars
+      }).catch(error => {
+        window.localStorage.removeItem("token");
+      })
+    }
   }
 
 };
